54eeae336c09e95ec8cf2ce49cec4fa92319e89e
Kendrick Chan <kenchan0824@gmail.com>
2023-05-26 18:44:48 +0100

    set status bar white

--- android/app/src/main/java/com/ancoatsoft/parking/MainActivity.java
+++ android/app/src/main/java/com/ancoatsoft/parking/MainActivity.java
@@ -1,5 +1,15 @@
 package com.ancoatsoft.parking;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      getWindow().setNavigationBarColor(Color.WHITE);
+  }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createMap, addMarker } from '../utils/googleMap';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 import {
   IonContent, IonPage, useIonViewDidEnter
@@ -10,9 +11,17 @@ const Home: React.FC = () => {
   const mapRef = useRef();
   let googleMap = null;
 
+  useEffect(() => {
+    StatusBar.setBackgroundColor({ color: "#FFFFFF" });
+    StatusBar.setStyle({ style: Style.Light });
+  }, []);
+
   useIonViewDidEnter(async () => {
     console.log('useIonViewDidEnter');
-    googleMap = await createMap(mapRef, {lat: 53.48262026858102, lng: -2.2181485479477163})
+    googleMap = await createMap(mapRef, {
+      lat: 53.48262026858102, 
+      lng: -2.2181485479477163
+    });
   });
 
   return (
